94fbaba31b248a9e29c59b0d748e063d57209b27
riccardotornesello <riccardo.tornesello@gmail.com>
2023-08-05 10:05:54 +0200

    install mantine

--- apps/goffredo-bot-platform/pages/_app.tsx
+++ apps/goffredo-bot-platform/pages/_app.tsx
@@ -1,16 +1,28 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import './styles.css';
+import { MantineProvider } from '@mantine/core';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>Welcome to goffredo-bot-platform!</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
       </Head>
-      <main className="app">
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: 'light',
+        }}
+      >
         <Component {...pageProps} />
-      </main>
+      </MantineProvider>
     </>
   );
 }
