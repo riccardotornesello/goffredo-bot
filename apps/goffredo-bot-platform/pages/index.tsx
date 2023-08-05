21b3f2afa995575a85e79d703b90f2265cf510d5
riccardotornesello <riccardo.tornesello@gmail.com>
2023-08-05 14:55:51 +0200

    add home title

--- apps/goffredo-bot-platform/pages/index.tsx
+++ apps/goffredo-bot-platform/pages/index.tsx
@@ -1,10 +1,11 @@
 import { generateDiscordOauthUrl } from '../utils/oauth2';
-import { Anchor } from '@mantine/core';
+import { Anchor, Title } from '@mantine/core';
 
 export function Index() {
   return (
     <div>
       <Anchor href={generateDiscordOauthUrl()}>Log in</Anchor>
+      <Title>A 💩 interface for a work in progress project</Title>
     </div>
   );
 }
